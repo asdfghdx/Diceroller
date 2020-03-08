51198981f7b4c348637a44952d98dab4c97ebd06
Ray <56859385+asdfghdx@users.noreply.github.com>
2020-03-08 12:41:12 +1100

    Completed application

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -28,13 +28,37 @@ public class MainActivity extends AppCompatActivity {
         tv2 = findViewById(R.id.tvDice2);
         tv3 = findViewById(R.id.tvSum);
 
-        iv1.setImageResource(getResources().getIdentifier("dice_1","drawable", "com.example.diceroll"));
-        iv2.setImageResource(getResources().getIdentifier("dice_2","drawable", "com.example.diceroll"));
+
+        iv1.setImageResource(getResources().getIdentifier("dice_1","drawable", "com.example.diceroller"));
+        iv2.setImageResource(getResources().getIdentifier("dice_2","drawable", "com.example.diceroller"));
 
         tv1.setText("1");
         tv2.setText("2");
         tv3.setText("3");
-        
 
+        btnDiceRoll.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int num1 = rollDice();
+                int num2 = rollDice();
+
+                int result1 = getResources().getIdentifier("dice_" + num1,"drawable","com.example.diceroller");
+                int result2 = getResources().getIdentifier("dice_" + num2,"drawable","com.example.diceroller");
+
+                iv1.setImageResource(result1);
+                iv2.setImageResource(result2);
+
+                tv1.setText(String.valueOf(num1));
+                tv2.setText(String.valueOf(num2));
+                tv3.setText(String.valueOf(num1+num2));
+
+            }
+        });
     }
+
+    //code for random number generator
+    public static int rollDice(){
+        return random.nextInt(6) + 1;
+    }
+
 }
